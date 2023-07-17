3d5e87a15402a5695c483ecea5663e1ab16a56a7
Nitheesh-Kumar99 <nitheeshrasta99@gmail.com>
2023-07-17 11:00:33 +0530

    new javascript practice

---
+++ Javascript.js
@@ -0,0 +1,39 @@
+// const countryOne = "India";
+// const continent = "Asia";
+// let populationOne = 16091999;
+
+// console.log(countryOne);
+// console.log(continent);
+// console.log(populationOne);
+
+// const isIsland = true;
+// let language = "Tamil";
+// console.log(typeof isIsland);
+// console.log(typeof populationOne);
+// console.log(typeof countryOne);
+// console.log(typeof language);
+
+// halfPopulationOne = populationOne / 2;
+// increasedPopulationOne = halfPopulationOne++;
+// console.log(halfPopulationOne);
+// console.log(increasedPopulationOne);
+
+// const countryTwo = "Finland";
+// let populationTwo = 600000;
+// compareTwoCountries = populationOne;
+
+// let description = `${countryOne} is in Asia and, its ${populationOne} people speak ${language}`;
+
+// console.log(description);
+
+// let markMass = 100;
+// let johnMass = 80;
+
+// let markHeight = 1.6;
+// let johnHeight = 2.2;
+
+// markBmi = markMass / markHeight ** 2;
+// johnBmi = johnMass / johnHeight ** 2;
+
+// let higherBmi = markBmi > johnBmi;
+// console.log(higherBmi, markBmi, johnBmi);
